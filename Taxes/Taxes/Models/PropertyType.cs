f844b095c197432e751492990775d5d2f216a01c
backs <jcfkbacks@gmail.com>
2016-06-10 19:43:44 -0500

    initial commit

---
+++ Taxes/Taxes/Models/PropertyType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taxes.Models
+{
+    public class PropertyType
+    {
+        [Key]
+        public int PropertyTypeId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        public string Description { get; set; }
+
+        public string Notes { get; set; }
+
+    }
+}
