66c86bf7ab5b91bea49aae655a110849d9547666
backs <jcfkbacks@gmail.com>
2016-06-13 12:26:01 -0500

    Automatic Migrations

--- Taxes/Taxes/Global.asax.cs
+++ Taxes/Taxes/Global.asax.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-
+using Taxes.Models;
+using Taxes.Migrations;
 namespace Taxes
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<TaxesContext, Configuration>());
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
